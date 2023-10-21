5069a21d7df107f2d3f7b5df925d20004939fa7f
zain saleem <117503756+zainsaleem7869@users.noreply.github.com>
2023-10-21 15:14:35 +0500

    Singly Linked LIst.cpp

--- Singly Linked LIst.cpp
+++ Singly Linked LIst.cpp
@@ -180,3 +180,250 @@ int main() {
 
     return 0;
 }
+
+// Delete Node at Start
+
+#include <iostream>
+
+// Define a struct for a linked list node
+struct Node {
+    int data;
+    Node* next;
+};
+
+class LinkedList {
+private:
+    Node* head; // Head of the linked list
+
+public:
+    LinkedList() : head(NULL) {}
+
+    // Function to add a node at the start of the linked list
+    void addToStart(int value) {
+        Node* newNode = new Node{value, NULL};
+        if (head != NULL) {
+            newNode->next = head;
+        }
+        head = newNode;
+    }
+
+    // Function to delete a node from the start of the linked list
+    void deleteFromStart() {
+        if (head != NULL) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    // Function to display the linked list
+    void display() {
+        Node* current = head;
+        while (current != NULL) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+
+    // Add nodes to the start
+    list.addToStart(1);
+    list.addToStart(2);
+    list.addToStart(3);
+
+    // Display the original linked list
+    std::cout << "Original Linked List: ";
+    list.display();
+
+    // Delete a node from the start
+    list.deleteFromStart();
+
+    // Display the modified linked list
+    std::cout << "Modified Linked List: ";
+    list.display();
+
+    return 0;
+}
+
+// Delete Node at END
+
+#include <iostream>
+
+// Define a struct for a linked list node
+struct Node {
+    int data;
+    Node* next;
+};
+
+class LinkedList {
+private:
+    Node* head; // Head of the linked list
+
+public:
+    LinkedList() : head(NULL) {}
+
+    // Function to add a node at the end of the linked list
+    void addToEnd(int value) {
+        Node* newNode = new Node{value, NULL};
+        if (head == NULL) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next != NULL) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    // Function to delete a node from the end of the linked list
+    void deleteFromEnd() {
+        if (head == NULL) {
+            return; // Empty list, nothing to delete
+        }
+
+        if (head->next == NULL) {
+            delete head;
+            head = NULL;
+            return;
+        }
+
+        Node* current = head;
+        while (current->next->next != NULL) {
+            current = current->next;
+        }
+
+        delete current->next;
+        current->next = NULL;
+    }
+
+    // Function to display the linked list
+    void display() {
+        Node* current = head;
+        while (current != NULL) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+
+    // Add nodes to the end
+    list.addToEnd(1);
+    list.addToEnd(2);
+    list.addToEnd(3);
+
+    // Display the original linked list
+    std::cout << "Original Linked List: ";
+    list.display();
+
+    // Delete a node from the end
+    list.deleteFromEnd();
+
+    // Display the modified linked list
+    std::cout << "Modified Linked List: ";
+    list.display();
+
+    return 0;
+}
+
+// Delete Node at Specifi Point
+
+#include <iostream>
+
+// Define a struct for a linked list node
+struct Node {
+    int data;
+    Node* next;
+};
+
+class LinkedList {
+private:
+    Node* head; // Head of the linked list
+
+public:
+    LinkedList() : head(NULL) {}
+
+    // Function to add a node at the end of the linked list
+    void addToEnd(int value) {
+        Node* newNode = new Node{value, NULL};
+        if (head == NULL) {
+            head = newNode;
+        } else {
+            Node* current = head;
+            while (current->next != NULL) {
+                current = current->next;
+            }
+            current->next = newNode;
+        }
+    }
+
+    // Function to delete a node at a specific point of the linked list
+    void deleteNodeAt(int position) {
+        if (head == NULL) {
+            return; // Empty list, nothing to delete
+        }
+
+        if (position == 0) {
+            Node* temp = head;
+            head = head->next;
+            delete temp;
+            return;
+        }
+
+        int currentPosition = 0;
+        Node* current = head;
+        while (current->next != NULL && currentPosition < position - 1) {
+            current = current->next;
+            currentPosition++;
+        }
+
+        if (current->next == NULL) {
+            return; // Node not found at the specified position
+        }
+
+        Node* temp = current->next;
+        current->next = temp->next;
+        delete temp;
+    }
+
+    // Function to display the linked list
+    void display() {
+        Node* current = head;
+        while (current != NULL) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+
+    // Add nodes to the end
+    list.addToEnd(1);
+    list.addToEnd(2);
+    list.addToEnd(3);
+
+    // Display the original linked list
+    std::cout << "Original Linked List: ";
+    list.display();
+
+    // Delete a node at a specific point
+    int positionToDelete = 1; // Change this to the desired position
+    list.deleteNodeAt(positionToDelete);
+
+    // Display the modified linked list
+    std::cout << "Modified Linked List: ";
+    list.display();
+
+    return 0;
+}

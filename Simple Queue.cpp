85bd21f1ec2b72628a946f966febc46f04d460c9
zain saleem <117503756+zainsaleem7869@users.noreply.github.com>
2023-10-21 15:10:18 +0500

    Simple Queue.cpp

--- Simple Queue.cpp
+++ Simple Queue.cpp
@@ -247,3 +247,159 @@ int main() {
 
     return 0;
 }
+
+// Add Node by Linked List
+
+#include <iostream>
+
+// Define a struct for a queue node
+struct Node {
+    int data;
+    Node* next;
+};
+
+class Queue {
+private:
+    Node* front; // Front of the queue
+    Node* rear;  // Rear of the queue
+
+public:
+    Queue() : front(NULL), rear(NULL) {}
+
+    // Function to add a node at the end of the queue
+    void enqueue(int value) {
+        Node* newNode = new Node;
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (front == NULL) {
+            front = newNode;
+            rear = newNode;
+        } else {
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+
+    // Function to display the queue
+    void display() {
+        if (front == NULL) {
+            return;
+        }
+
+        Node* current = front;
+        while (current != NULL) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    Queue queue;
+
+    // Add nodes to the end
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.enqueue(4);
+
+    // Display the queue
+    queue.display();
+
+    return 0;
+}
+
+// Delete Node by Linked List
+
+#include <iostream>
+
+// Define a struct for a queue node
+struct Node {
+    int data;
+    Node* next;
+};
+
+class Queue {
+private:
+    Node* front; // Front of the queue
+    Node* rear;  // Rear of the queue
+
+public:
+    Queue() : front(NULL), rear(NULL) {}
+
+    // Function to add a node at the end of the queue
+    void enqueue(int value) {
+        Node* newNode = new Node;
+        newNode->data = value;
+        newNode->next = NULL;
+
+        if (front == NULL) {
+            front = newNode;
+            rear = newNode;
+        } else {
+            rear->next = newNode;
+            rear = newNode;
+        }
+    }
+
+    // Function to remove a node from the front of the queue
+    int dequeue() {
+        if (front == NULL) {
+            std::cout << "Queue is empty." << std::endl;
+            return -1; // Return a sentinel value
+        }
+
+        int data = front->data;
+        Node* temp = front;
+        front = front->next;
+
+        if (front == NULL) {
+            rear = NULL;
+        }
+
+        delete temp;
+        return data;
+    }
+
+    // Function to display the queue
+    void display() {
+        if (front == NULL) {
+            return;
+        }
+
+        Node* current = front;
+        while (current != NULL) {
+            std::cout << current->data << " ";
+            current = current->next;
+        }
+        std::cout << std::endl;
+    }
+};
+
+int main() {
+    Queue queue;
+
+    // Add nodes to the end
+    queue.enqueue(1);
+    queue.enqueue(2);
+    queue.enqueue(3);
+    queue.enqueue(4);
+
+    // Display the original queue
+    std::cout << "Original Queue: ";
+    queue.display();
+
+    // Delete a node from the front
+    int deletedValue = queue.dequeue();
+    if (deletedValue != -1) {
+        std::cout << "Deleted Node: " << deletedValue << std::endl;
+    }
+
+    // Display the modified queue
+    std::cout << "Modified Queue: ";
+    queue.display();
+
+    return 0;
+}
